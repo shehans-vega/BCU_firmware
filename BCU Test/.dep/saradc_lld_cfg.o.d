6ef80c2c9ffc8af409e6dd5caac8435733e00969
shehan <shehans@vega.lk>
2024-09-20 00:28:30 -0700

    verbose off

---
+++ BCU Test/.dep/saradc_lld_cfg.o.d
@@ -0,0 +1,95 @@
+build/obj/saradc_lld_cfg.o: \
+ components/spc582bxx_low_level_drivers_component_rla/cfg/saradc_lld_cfg.c \
+ components/spc582bxx_low_level_drivers_component_rla/cfg/saradc_lld_cfg.h \
+ components/spc582bxx_low_level_drivers_component_rla/lib/include/spc5_lld.h \
+ components/spc582bxx_platform_component_rla/lib/include/platform.h \
+ components/spc582bxx_platform_component_rla/cfg/core.h \
+ components/spc582bxx_platform_component_rla/cfg/toolchain.h \
+ components/spc582bxx_platform_component_rla/lib/include/intc.h \
+ components/spc582bxx_platform_component_rla/lib/include/typedefs.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\lib\gcc\powerpc-eabivle\4.9.4\include\stdint.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\stdint.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\machine\_default_types.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\sys\features.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\sys\_intsup.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\lib\gcc\powerpc-eabivle\4.9.4\include\stddef.h \
+ c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\lib\gcc\powerpc-eabivle\4.9.4\include\stdbool.h \
+ components/spc582bxx_platform_component_rla/lib/include/spr.h \
+ components/spc582bxx_platform_component_rla/lib/include/spc582b_registry.h \
+ components/spc582bxx_platform_component_rla/lib/include/spc582b.h \
+ components/spc582bxx_platform_component_rla/lib/include/typedefs.h \
+ components/spc582bxx_board_initialization_component_rla/cfg/board.h \
+ components/spc582bxx_board_initialization_component_rla/lib/include/pal.h \
+ components/spc582bxx_clock_component_rla/lib/include/clock.h \
+ components/spc582bxx_irq_component_rla/lib/include/irq.h \
+ components/spc582bxx_irq_component_rla/lib/include/vectors.h \
+ components/spc582bxx_irq_component_rla/cfg/irq_cfg.h \
+ components/spc582bxx_clock_component_rla/cfg/clock_cfg.h \
+ components/spc582bxx_osal_component_rla/lib/include/osal.h \
+ components/spc582bxx_osal_component_rla/cfg/osal_cfg.h \
+ components/spc582bxx_osal_component_rla/lib/include/systimer.h \
+ components/spc582bxx_low_level_drivers_component_rla/cfg/lldconf.h \
+ components/spc582bxx_low_level_drivers_component_rla/lib/include/saradc_lld.h \
+ components/spc582bxx_low_level_drivers_component_rla/lib/include/spc5_lld.h
+
+components/spc582bxx_low_level_drivers_component_rla/cfg/saradc_lld_cfg.h:
+
+components/spc582bxx_low_level_drivers_component_rla/lib/include/spc5_lld.h:
+
+components/spc582bxx_platform_component_rla/lib/include/platform.h:
+
+components/spc582bxx_platform_component_rla/cfg/core.h:
+
+components/spc582bxx_platform_component_rla/cfg/toolchain.h:
+
+components/spc582bxx_platform_component_rla/lib/include/intc.h:
+
+components/spc582bxx_platform_component_rla/lib/include/typedefs.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\lib\gcc\powerpc-eabivle\4.9.4\include\stdint.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\stdint.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\machine\_default_types.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\sys\features.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\powerpc-eabivle\include\sys\_intsup.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\lib\gcc\powerpc-eabivle\4.9.4\include\stddef.h:
+
+c:\spc5studio-6.0\eclipse\plugins\com.st.tools.spc5.tools.gnu.gcc.ppcvle.win32_4.9.4.20200908161514\toolchain\lib\gcc\powerpc-eabivle\4.9.4\include\stdbool.h:
+
+components/spc582bxx_platform_component_rla/lib/include/spr.h:
+
+components/spc582bxx_platform_component_rla/lib/include/spc582b_registry.h:
+
+components/spc582bxx_platform_component_rla/lib/include/spc582b.h:
+
+components/spc582bxx_platform_component_rla/lib/include/typedefs.h:
+
+components/spc582bxx_board_initialization_component_rla/cfg/board.h:
+
+components/spc582bxx_board_initialization_component_rla/lib/include/pal.h:
+
+components/spc582bxx_clock_component_rla/lib/include/clock.h:
+
+components/spc582bxx_irq_component_rla/lib/include/irq.h:
+
+components/spc582bxx_irq_component_rla/lib/include/vectors.h:
+
+components/spc582bxx_irq_component_rla/cfg/irq_cfg.h:
+
+components/spc582bxx_clock_component_rla/cfg/clock_cfg.h:
+
+components/spc582bxx_osal_component_rla/lib/include/osal.h:
+
+components/spc582bxx_osal_component_rla/cfg/osal_cfg.h:
+
+components/spc582bxx_osal_component_rla/lib/include/systimer.h:
+
+components/spc582bxx_low_level_drivers_component_rla/cfg/lldconf.h:
+
+components/spc582bxx_low_level_drivers_component_rla/lib/include/saradc_lld.h:
+
+components/spc582bxx_low_level_drivers_component_rla/lib/include/spc5_lld.h:
