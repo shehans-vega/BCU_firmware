21411bf4bbd98e173e9d57fb08db5ebeb15b142f
shehan <shehans@vega.lk>
2024-09-26 03:47:09 -0700

    created a evaluate press method

--- BCU Test/source/APP/device_class.hpp
+++ BCU Test/source/APP/device_class.hpp
@@ -9,82 +9,159 @@
 #define lowbeam_current 1
 #define COUNTER_THRESHOLD 40
 
-class Device{
+class Device
+{
 
-    private:
-  
-    public:
-        Channel* device_channel;
-        buttonInput_t* device_button;
-        uint8_t max_current;
-        uint8_t counter;
-        bool state;
+private:
+public:
+    Channel *device_channel;
+    buttonInput_t *device_button;
+    uint8_t max_current;
+    uint8_t counter;
+    bool state;
 
-        Device();
-        Device(Channel* channel, buttonInput_t* button,uint8_t current): device_channel(channel),device_button(button),max_current(current){
-            state = 1;
-        };
-        virtual void activate() = 0;
-    bool fault;
+    Device();
+    Device(Channel *channel, buttonInput_t *button, uint8_t current) : device_channel(channel), device_button(button), max_current(current)
+    {
+        state = 1;
     };
-
-class momentary_Device:public Device{
-    public:
-        momentary_Device(Channel* channel, buttonInput_t* button,uint8_t current): Device( channel,  button, current){
-            state = 0; counter = 0;
-        }
-        void activate() override{
-            if ((this->device_button->buttonState == BUTTON_PRESSED) || (this->device_button->buttonState == BUTTON_HOLD)) {
-        this->state = 1;
-    } else {
-        this->state = 0;
-    }
-    if (this->state == 1) {
-           this->device_channel->channel_on_impl();
-       } else {
-           this->device_channel->channel_off_impl();
-       }
-        }      
+    virtual void activate() = 0;
+    virtual bool evaluate_press() = 0;
+    bool fault;
 };
 
-class latch_Device:public Device{
-    public:
-     latch_Device(Channel* channel, buttonInput_t* button,uint8_t current): Device( channel,  button, current){
-        state = 0; counter = 0;
-     }
-     void activate() override{
+class momentary_Device : public Device
+{
+public:
+    momentary_Device(Channel *channel, buttonInput_t *button, uint8_t current) : Device(channel, button, current)
+    {
+        state = 0;
+        counter = 0;
+    }
+    bool evaluate_press() override
+    {
+        if ((this->device_button->buttonState == BUTTON_PRESSED) || (this->device_button->buttonState == BUTTON_HOLD))
+        {
+            return true;
         }
+        else
+        {
+            return false;
+        }
+    }
+    void activate() override
+    {
+        if (this->evaluate_press())
+        {
+            this->state = 1;
+        }
+        else
+        {
+            this->state = 0;
+        }
+        if (this->state == 1)
+        {
+            this->device_channel->channel_on_impl();
+        }
+        else
+        {
+            this->device_channel->channel_off_impl();
+        }
+    }
 };
 
-    class toggle_Device:public Device{
-    public:
-    toggle_Device(Channel* channel, buttonInput_t* button,uint8_t current): Device( channel,  button, current){
-        state = 0; counter = 0;
+class latch_Device : public Device
+{
+public:
+    latch_Device(Channel *channel, buttonInput_t *button, uint8_t current) : Device(channel, button, current)
+    {
+        state = 0;
+        counter = 0;
     }
-    void activate() override{
+    bool evaluate_press() override
+    {
+        if ((this->device_button->buttonState == BUTTON_RELEASED) && (this->device_button->buttonPrevState == (BUTTON_PRESSED || BUTTON_HOLD)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void activate() override
+    {
+
+        if (this->evaluate_press())
+        {
+            this->state = !this->state;
+        }
+
+        if (this->state == 1)
+        {
+            this->device_channel->channel_on_impl();
+        }
+        else
+        {
+            this->device_channel->channel_off_impl();
+        }
+    }
+};
+
+class toggle_Device : public Device
+{
+public:
+    toggle_Device(Channel *channel, buttonInput_t *button, uint8_t current) : Device(channel, button, current)
+    {
+        state = 0;
+        counter = 0;
+    }
+
+    bool evaluate_press() override
+    {
+        if ((this->device_button->buttonState == BUTTON_RELEASED) && (this->device_button->buttonPrevState == (BUTTON_PRESSED || BUTTON_HOLD)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void activate() override
+    {
         static bool pulse = false;
 
-	 if ((this->device_button->buttonState == BUTTON_RELEASED) && (this->device_button->buttonPrevState == (BUTTON_PRESSED||BUTTON_HOLD))) {
-			pulse = !pulse; // Toggle the state
-		}
-
-	if(pulse){
-    if (this->counter >= COUNTER_THRESHOLD) {
-        this->counter = 0;
-        this->state = !this->state; // Toggle the state
-    }
-    this->counter++;
-	}else{
-		this->state = 0;
-	}
-
-    if (this->state == 1) {
-        this->device_channel->channel_on_impl();
-    } else {
-        this->device_channel->channel_off_impl();
-    }
-
+        if (this->evaluate_press())
+        {
+            pulse = !pulse; // Toggle the state
         }
+
+        if (pulse)
+        {
+            if (this->counter >= COUNTER_THRESHOLD)
+            {
+                this->counter = 0;
+                this->state = !this->state; // Toggle the state
+            }
+            this->counter++;
+        }
+        else
+        {
+            this->state = 0;
+        }
+
+        if (this->state == 1)
+        {
+            this->device_channel->channel_on_impl();
+        }
+        else
+        {
+            this->device_channel->channel_off_impl();
+        }
+    }
 };
 
 #endif
