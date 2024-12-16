4cdfa9ada9a51f94c9de6c3daf50166118f1cfca
shehans-vega <shehans@vega.lk>
2024-12-16 00:52:02 -0800

    changed left right signal channels

--- BCU Test/source/APP/devices.hpp
+++ BCU Test/source/APP/devices.hpp
@@ -20,9 +20,9 @@ void create_devices() {
     // Initialize devices at runtime
     devices[horn] = new momentary_Device(&channels[3], &(buttons[HRN_BTN]), 10 , 15, 2000);
     devices[lbeam] = new latch_Device(&channels[0], &hbeam_btn, 10,15,2000);
-    devices[pass] = new momentary_Device(&channels[4], &(buttons[PASS_BTN]), 10,15,2000);
-    devices[left] = new toggle_Device(&channels[1], &(buttons[LEFT_BTN]), 10,15,2000);
-    devices[right] = new toggle_Device(&channels[2], &(buttons[RGT_BTN]), 10,15,2000);
+    devices[pass] = new momentary_Device(&channels[5], &(buttons[PASS_BTN]), 10,15,2000);
+    devices[left] = new toggle_Device(&channels[2], &(buttons[LEFT_BTN]), 10,15,2000);
+    devices[right] = new toggle_Device(&channels[1], &(buttons[RGT_BTN]), 10,15,2000);
     devices[hbeam] = new momentary_Device(&channels[5], &hbeam_btn, 5,15,300);
 } 
 
